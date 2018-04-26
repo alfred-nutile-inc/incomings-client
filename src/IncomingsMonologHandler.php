43463f5be89fd588914818e8ec64a00114a53210
Luis Dalmolin <luis.nh@gmail.com>
2018-04-26 09:35:41 -0300

    Added IncomingsMonologHandler for laravel 5.6 logging

---
+++ src/IncomingsMonologHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AlfredNutileInc\Incomings;
+
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+class IncomingsHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var AlfredNutileInc\Incomings\IncomingsLoggerProvider
+     */
+    protected $incomings;
+
+    public function __construct($level = Logger::DEBUG, $bubble = true)
+    {
+        parent::__construct($level, $bubble);
+
+        $this->incomings = app('incomings_logger');
+    }
+
+    /**
+     * Write the log message.
+     *
+     * @param array $data
+     * @return void
+     */
+    protected function write(array $record)
+    {
+        $this->send($record);
+    }
+
+    /**
+     * Send the log to incomings.
+     *
+     * @param array $data
+     * @return void
+     */
+    protected function send($data)
+    {
+        $level = strtolower($data['level_name']);
+        $this->incomings->$level($data['message']);
+    }
+}
